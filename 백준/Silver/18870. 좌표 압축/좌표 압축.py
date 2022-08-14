d128d92aa4cf116444094aa84d8a0c62cb80d47a
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-08-14 20:27:52 +0900

    [Silver II] Title: 좌표 압축, Time: 1508 ms, Memory: 283904 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,15 +1,19 @@
 # 220814 18870 좌표 압축
 
+# 입력값 받기
 N = int(input())
+coordinate = list(map(int, input().split()))
 
-nums = list(map(int, input().split()))
-nums_no_same = list(set(nums))
-nums_no_same.sort()
-nums_dict = {}
+# 중복값을 제거하고 정렬해준다.
+no_same = list(set(coordinate))
+no_same.sort()
 
-for i in range(len(nums_no_same)):
-    nums_dict[nums_no_same[i]] = i
+# 압축된 좌표를 담을 딕셔너리
+compress = {}
+# 압축된 좌표를 key로, 원래 좌표를 value로 하는 딕셔너리를 만든다.
+for i in range(len(no_same)):
+    compress[no_same[i]] = i
 
-
-for i in nums:
-    print(nums_dict[i], end=' ')
+# 양식에 맞게 압축된 좌표를 출력해준다.
+for i in coordinate:
+    print(compress[i], end=' ')
