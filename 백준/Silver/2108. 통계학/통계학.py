0791ace86a356f94da8f17cba43f3c280735e1f6
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-08-14 21:50:53 +0900

    [Silver III] Title: 통계학, Time: 376 ms, Memory: 150644 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,17 +1,18 @@
+# 220814 2108 통계학
+
 import sys
 from collections import Counter
 
 input = sys.stdin.readline
 
 N = int(input())
-
 nums = []
-
 for _ in range(N):
     nums.append(int(input()))
 
 nums.sort()
 
+# 최빈값 구하기
 if len(nums) == 1:
     mode = nums[0]
 else:
@@ -21,23 +22,7 @@ else:
     else:
         mode = temp[0][0]
 
-
-# print(temp)
-#
-# if len(nums) == 1:
-#     mode = nums[0]
-# else:
-#     temp = {}
-#     for i in nums:
-#         temp[i] = nums.count(i)
-#
-#     temp = sorted(temp.items(), key=lambda x: x[1], reverse=True)
-#     if temp[0][1] == temp[1][1]:
-#         mode = temp[1][0]
-#     else:
-#         mode = temp[0][0]
-
-
+# 최댓값, 중간값, 범위 구하기
 average = round(sum(nums) / len(nums))
 mean = nums[len(nums)//2]
 num_range = nums[-1] - nums[0]
