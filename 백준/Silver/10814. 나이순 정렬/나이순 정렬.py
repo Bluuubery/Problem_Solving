1162826ac082d40f0336fb341528f832c1da4ccd
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-08-15 20:42:25 +0900

    [Silver V] Title: 나이순 정렬, Time: 324 ms, Memory: 138144 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -11,7 +11,9 @@ members = []
 for _ in range(N):
     members.append(input().split())
 
+# 파이썬의 기본 .sort 메소드(팀소트)는 안정정렬이므로 따로 작업을 해줄 필요가 없다.
 members.sort(key= lambda x: int(x[0]))
 
 for member in members:
     print(*member)
+
