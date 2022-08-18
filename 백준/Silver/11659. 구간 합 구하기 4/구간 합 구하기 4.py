6464767229c3de09c18dbee00827c25252a87edc
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-08-18 23:31:43 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 272 ms, Memory: 40596 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -8,12 +8,12 @@ N, M = map(int, input().split())
 
 numbers = list(map(int, input().split()))
 
+# 누적합 리스트를 먼저 만들어 둔다.
 sum_numbers= [0, numbers[0]]
-
 for i in range(1, N):
     sum_numbers.append(numbers[i] + sum_numbers[-1])
 
-
+# 인덱스를 통해 바로 접근한다. (시간복잡도 O(1))
 for _ in range(M):
     i, j = map(int, input().split())
     print(sum_numbers[j] - sum_numbers[i - 1])
