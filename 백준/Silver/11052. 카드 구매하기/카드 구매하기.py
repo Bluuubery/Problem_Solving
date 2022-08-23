bdd42f3f9eb5049dd7fd32ce323a216b69e43464
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-08-23 22:53:18 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -4,13 +4,17 @@ import sys
 
 input = sys.stdin.readline
 
+# N: 구매할 카드 개수, card: 카드팩 가격 
 N = int(input())
 card = [0] + list(map(int, input().split()))
 
+# dp 저장 테이블
 dp = [0 for _ in range(N + 1)]
 
+# dp[i] = max(dp[i], dp[i - k] + card[k])
 for i in range(N + 1):
     for k in range(i + 1):
         dp[i] = max(dp[i], dp[i - k] + card[k])
 
+# 정답 출력
 print(dp[i])
