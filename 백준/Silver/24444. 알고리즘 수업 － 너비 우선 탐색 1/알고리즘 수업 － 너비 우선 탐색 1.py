270c799bb56b8fa6907430314998951fd05d14fe
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-08-25 23:32:53 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 428 ms, Memory: 135572 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -6,14 +6,19 @@ import sys
 input = sys.stdin.readline
 
 
+# bfs 함수 선언
 def bfs(V, E, R):
     global cnt
     
+    # 방문 표시 후 큐에 삽입
     visited[R] = cnt
     q.append(R)
 
+    # 큐가 빌 때까지
     while q:
+        # 큐에서 꺼낸 곳부터
         u = q.popleft()
+        # 미방문 인접노드 탐색
         for v in E[u]:
             if visited[v] == 0:
                 cnt += 1
@@ -37,12 +42,14 @@ for _ in range(M):
 for edge in E:
     edge.sort()
 
-# visited: 방문 기록용 리스트, cnt: 방문 순서
+# visited: 방문 기록용 리스트, q: bfs 경로 저장용, cnt: 방문 순서
 visited = [0 for _ in range(N + 1)]
 q = deque()
 cnt = 1
 
+# bfs 탐색 실시
 bfs(N, E, R)
 
+# 방문 순서 출력
 for i in range(1, N + 1):
     print(visited[i])
