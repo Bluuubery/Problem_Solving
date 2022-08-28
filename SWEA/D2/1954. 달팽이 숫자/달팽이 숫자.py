e29b59138244eb878e2daca2f87552a7b9687b30
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-08-28 13:09:54 +0900

    [D2] Title: 달팽이 숫자, Time: 149 ms, Memory: 56,684 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,40 +1,42 @@
+# 220828 1954 달팽이 숫자
+
+# 정답코드
+
 T = int(input())
 
-# 달팽이 진행 방향(우 -> 하 -> 좌 -> 상)에 맞게 델타 list 만들기
-di = [0, 1, 0, -1] 
-dj = [1, 0, -1, 0]
-
 for t in range(1, T + 1):
-    N = int(input())
-    # 달팽이 숫자를 담을 빈 이차원 배열 반들기
-    snail = [[0] * N for _ in range(N)]
-
-    # 초기 좌표 설정
-    i = 0
-    j = 0
-
-    # 이동 방향 설정
-    direction = 0 
-
-    for n in range(1, N*N + 1):
-        # 위치에 맞는 달팽이 숫자를 넣어주고 좌표를 이동 시킨다.
-        snail[i][j] = n
-        i += di[direction]
-        j += dj[direction]
-
-        # 좌표가 범위를 벗어나거나 해당 좌표에 이미 숫자가 있을 경우
-        if not (0 <= i < N and 0 <= j < N and snail[i][j]==0):
-            # 좌표 이동을 취소한다.
-            i -= di[direction]
-            j -= dj[direction]
-            # 진행 방향을 바꿔준다.
-            direction = (direction + 1) % 4
-            # 진행 방향에 맞는 새로운 좌표를 설정해준다.
-            i += di[direction]
-            j += dj[direction]
     
-    # 정답을 양식에 맞게 출력해준다.
-    print("#{}".format(t))
+    # N: 달팽이 숫자 크기, arr: 달팽이 숫자를 담을 빈 배열
+    N = int(input())
+    arr = [[0] * N for _ in range(N)]
 
-    for row in snail:
-        print(*row)
+    # 탐색 방향 (달팽이의 이동 방향: 우 -> 하 -> 좌 -> 상)
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+
+    # r, c, d: 달팽이의 출발점 및 출발 방향
+    r = 0
+    c = 0
+    d = 0
+
+    # 1부터 N^2까지의 숫자를 채워 넣는다.
+    for i in range(1, N**2 + 1):
+        arr[r][c] = i
+        
+        # nr, nc: 다음 탐색 방향
+        nr = r + dr[d]
+        nc = c + dc[d]
+
+        # 범위를 벗어나지 않고 0이면 다음 탐색 방향으로 이동
+        if 0<= nr < N and 0 <= nc < N and arr[nr][nc] == 0:
+            r, c = nr, nc
+        # 범위를 벗어나거나 이미 다른 숫자가 있으면 방향 전환
+        else:
+            d = (d + 1) % 4
+            r += dr[d]
+            c += dc[d]         
+
+    # 달팽이 숫자 출력
+    print(f'#{t} ')
+    for row in arr:
+        print(*row)             
