8b26eb4a785477987402a3abd3bb74e45db25029
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-08-29 11:20:14 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 392 ms, Memory: 117116 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -2,7 +2,6 @@
 
 # 정답코드
 
-from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -41,6 +40,7 @@ def spread_dust(room):
                 for i in range(4):
                     nr = r + dr[i]
                     nc = c + dc[i]
+                    # 인덱스를 벗어나지 않고 공기청정기가 아닌 경우에 미세먼지 확산
                     if 0 <= nr < R and 0 <= nc < C and room[nr][nc] != -1:
                         dust_room[nr][nc] += room[r][c] // 5
                         cnt += 1
@@ -58,9 +58,11 @@ def air_clean(room, top, bottom):
     
     # 위쪽 공기 순환
     def top_circuleate():
+        # 공기청정기가 먼지를 한칸씩 빨아들이는 식으로 구현(임시 변수 사용x)
+        # 공기 청정기 윗칸부터 역순으로 탐색
         dr = [-1, 0, 1, 0]
         dc = [0, 1, 0, -1]
-        # 공기청정기가 먼지를 한칸씩 빨아들이는 식으로 접근
+        # r, c: 시작 위치, d: 시작 방향
         r = top - 1
         c = 0
         d = 0
@@ -69,21 +71,23 @@ def air_clean(room, top, bottom):
             nr = r + dr[d]
             nc = c + dc[d]
             if 0 <= nr <= top and 0 <= nc < C:
+                # 공기청정기를 만나게 되면 해당 영역을 0으로 바꿔주고(공기청정기에 의해 밀려남) 순환을 종료한다.
                 if room[nr][nc] == -1:
                     room[r][c] = 0
-                    return 
+                    return
+                # 밀려난 미세먼지 표시해주고 좌표 이동
                 else:
                     room[r][c] = room[nr][nc]
                     r = nr
                     c = nc
+            # 인덱스를 벗어나면 방향 변경
             else:
                 d += 1
 
-    # 아래쪽 공기 순환
+    # 아래쪽 공기 순환 (위쪽과 동일한 프로세스)
     def bottom_circuleate():
         dr = [1, 0, -1, 0]
         dc = [0, 1, 0, -1]
-        # 공기청정기 다음 칸부터 순환
         r = bottom + 1
         c = 0
         d = 0
@@ -102,8 +106,10 @@ def air_clean(room, top, bottom):
             else:
                 d += 1
 
+    # 공기청정기 가동
     top_circuleate()
     bottom_circuleate()
+
     return room
     
 
