0602b567a57447d42aeca07df2c5945ea5252c22
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-05 00:15:16 +0900

    [Silver V] Title: 뒤집기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -2,8 +2,12 @@
 
 # 정답코드
 
-S = input()
+import sys
+input = sys.stdin.readline
 
+S = input().strip()
+
+# str_0: 연속된 0으로 구성된 문자열의 개수, str_1: 연속된 1로 구성된 문자열의 개수
 if S[0] == '0':
     str_0 = 1
     str_1 = 0
@@ -11,6 +15,7 @@ else:
     str_0 = 0
     str_1 = 1
 
+# 문자가 바뀌면 다음에 오는 문자에 따라 문자열을 세준다.
 for i in range(len(S) - 1):
     if S[i] != S[i + 1]:
         if S[i + 1] == '0':
@@ -18,4 +23,5 @@ for i in range(len(S) - 1):
         else:
             str_1 += 1
 
+# 두 문자열 중 적은 것의 개수 출력
 print(min(str_0, str_1))
