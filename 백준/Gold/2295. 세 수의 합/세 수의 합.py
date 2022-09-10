90abe57a7434dc56e86b1c7eb4fd24272590d7c7
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-10 21:07:04 +0900

    [Gold IV] Title: 세 수의 합, Time: 576 ms, Memory: 214648 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -1,41 +1,37 @@
 # 220910 2295 세 수의 합
 
-# 정답코드
+# 정답코드 (딕셔너리)
 
+from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
-
-def binary_search(target, arr, left, right):
-    if left > right:
-        return False
-    
-    mid = (left + right) // 2
-    if target == arr[mid]:
-        return True
-    elif target > arr[mid]:
-        return binary_search(target, arr, mid + 1, right)
-    else:
-        return binary_search(target, arr, left, mid - 1)
-
-
+# N: 수의 개수, numbers: 수열
 N = int(input())
 numbers = []
 for _ in range(N):
-    numbers.append(int(input()))
+    number = int(input())
+    numbers.append(number)
 
+# two_sum: numbers 리스트의 두 원소의 합들을 담은 배열
 two_sum = []
+two_sum_dict = defaultdict(int)
 
 for i in range(N):
     for j in range(N):
-        two_sum.append(numbers[i] + numbers[j])
+        sum_num = numbers[i] + numbers[j]
+        two_sum.append(sum_num)
+        two_sum_dict[sum_num] += 1
 
+# 두 리스트를 정렬해준다.
 two_sum.sort()
+# 최댓값을 찾아야 하므로 numbers는 내림차순으로 정렬
 numbers.sort(reverse=True)
 
+# target: 찾고자 하는 수
 for i in range(N):
     target = numbers[i]
     for j in range(N):
-        if binary_search(target - numbers[j], two_sum, 0, len(two_sum) - 1):
+        if two_sum_dict[target - numbers[j]]:
             print(target)
             exit(0)
