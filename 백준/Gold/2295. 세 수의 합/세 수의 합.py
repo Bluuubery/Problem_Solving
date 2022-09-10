8fd0fa2adc72089b71ac65a3ede35580cad71f6a
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-10 21:09:13 +0900

    [Gold IV] Title: 세 수의 합, Time: 352 ms, Memory: 188112 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -13,22 +13,20 @@ for _ in range(N):
     number = int(input())
     numbers.append(number)
 
-# two_sum: numbers 리스트의 두 원소의 합들을 담은 배열
-two_sum = []
+# two_sum_dict: numbers 리스트의 두 원소의 합을 키로, 등장횟수를 밸류로 하는 딕셔너리
 two_sum_dict = defaultdict(int)
 
 for i in range(N):
     for j in range(N):
         sum_num = numbers[i] + numbers[j]
-        two_sum.append(sum_num)
         two_sum_dict[sum_num] += 1
 
-# 두 리스트를 정렬해준다.
-two_sum.sort()
+
 # 최댓값을 찾아야 하므로 numbers는 내림차순으로 정렬
 numbers.sort(reverse=True)
 
 # target: 찾고자 하는 수
+# 딕셔너리를 통해서 target - numbers[j]가 two_sum_dict에 존재하는지 확인한다.
 for i in range(N):
     target = numbers[i]
     for j in range(N):
