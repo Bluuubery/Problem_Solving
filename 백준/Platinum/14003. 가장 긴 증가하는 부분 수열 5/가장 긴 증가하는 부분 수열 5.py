f17d1ab622c89eaf33caca8b9913cb4d7243f5ca
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-12 16:42:10 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 788 ms, Memory: 265196 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -17,7 +17,8 @@ input = sys.stdin.readline
 N = int(input())
 numbers = list(map(int, input().split()))
 
-
+# lis_temp: 임시 lis
+# lis_idx: 각 수의 lis에서의 idx를 담을 리스트
 lis_temp = []
 lis_idx = []
 
@@ -26,14 +27,14 @@ for i in range(N):
     # idx: 수가 들어갈 위치
     idx = bisect_left(lis_temp, numbers[i])
     lis_idx.append(idx)
-    # 현재 lis의 길이보다 뒤쪽이면 lis에 추가해준다.
+    # 현재 lis의 길이보다 뒤쪽이면 임시 lis에 추가해준다.
     if len(lis_temp) <= idx:
         lis_temp.append(numbers[i])
     # 들어갈 위치에 있는 수와 대체
     else:
         lis_temp[idx] = numbers[i]
 
-# print(lis_idx)
+# lis_idx에 저장해둔 인덱스를 거꾸로 탐색해서 lis를 채워나간다.
 max_idx = max(lis_idx)
 lis = []
 for i in range(N - 1, -1, -1):
@@ -41,7 +42,9 @@ for i in range(N - 1, -1, -1):
         lis.append(numbers[i])
         max_idx -= 1
 
+# 거꾸로된 lis를 다시 정방향으로 뒤집어준다.
 lis.reverse()
 
+# lis의 길이, lis 출력
 print(len(lis))
 print(*lis)
