ff472cb3b7d3417fbd14482f37bc940095d0042f
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-13 09:34:39 +0900

    [Gold V] Title: 적록색약, Time: 120 ms, Memory: 32492 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -9,20 +9,20 @@ input = sys.stdin.readline
 
 
 N = int(input())
-arr = [list(map(str, input().strip())) for _ in range(N)]
-arr_rgb = [[0 for _ in range(N)] for _ in range(N)]
+arr_rgb = [list(map(str, input().strip())) for _ in range(N)]
+arr_no_rg = [[0 for _ in range(N)] for _ in range(N)]
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 'B':
-            arr_rgb[i][j] = 'B'
+        if arr_rgb[i][j] == 'B':
+            arr_no_rg[i][j] = 'B'
         else:
-            arr_rgb[i][j] = 'R'
+            arr_no_rg[i][j] = 'R'
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
 
-def bfs(r, c, color, cnt):
+def bfs(r, c, arr, color, cnt):
     queue = deque()
     arr[r][c] = cnt
     queue.append((r, c))
@@ -39,33 +39,17 @@ def bfs(r, c, color, cnt):
                     queue.append((nr, nc))
 
 
-def bfs_rgb(r, c, color, cnt):
-    queue = deque()
-    arr_rgb[r][c] = cnt
-    queue.append((r, c))
 
-    while queue:
-        r, c = queue.popleft()
-
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < N:
-                if arr_rgb[nr][nc] == color:
-                    arr_rgb[nr][nc] = cnt
-                    queue.append((nr, nc))
-
-
-cnt = 0
 cnt_rgb = 0
+cnt_no_rg = 0
 for r in range(N):
     for c in range(N):
-        if arr[r][c] == 'R' or arr[r][c] == 'G' or arr[r][c] == 'B':
-            bfs(r, c, arr[r][c], cnt)
-            cnt += 1
-
         if arr_rgb[r][c] == 'R' or arr_rgb[r][c] == 'G' or arr_rgb[r][c] == 'B':
-            bfs_rgb(r, c, arr_rgb[r][c], cnt_rgb)
+            bfs(r, c, arr_rgb, arr_rgb[r][c], cnt_rgb)
             cnt_rgb += 1
 
-print(cnt, cnt_rgb)
+        if arr_no_rg[r][c] == 'R' or arr_no_rg[r][c] == 'G' or arr_no_rg[r][c] == 'B':
+            bfs(r, c, arr_no_rg, arr_no_rg[r][c], cnt_rgb)
+            cnt_no_rg += 1
+
+print(cnt_rgb, cnt_no_rg)
