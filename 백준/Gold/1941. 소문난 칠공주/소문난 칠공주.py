5d99e6bd3b6217f9b9614d59a7251ccb703ecba4
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-18 12:44:24 +0900

    [Gold III] Title: 소문난 칠공주, Time: 3704 ms, Memory: 32508 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -3,9 +3,9 @@ from itertools import count
 import sys, os, io, atexit
 
 input = lambda: sys.stdin.readline().rstrip('\r\n')
-stdout = io.BytesIO()
-sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
-atexit.register(lambda: os.write(1, stdout.getvalue()))
+# stdout = io.BytesIO()
+# sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+# atexit.register(lambda: os.write(1, stdout.getvalue()))
 
 # 220918 1941 소문난 칠공주
 
