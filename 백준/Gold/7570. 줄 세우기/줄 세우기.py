4914ceb5b3abb2d83b28bda9558322d8c6a1216b
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-21 23:39:10 +0900

    [Gold III] Title: 줄 세우기, Time: 512 ms, Memory: 257484 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.py
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.py
@@ -9,14 +9,22 @@ atexit.register(lambda: os.write(1, stdout.getvalue()))
 
 # 정답코드
 
+# N: 아이들 명 수
 N = int(input())
+# children: 아이들 번호
 children = list(map(int, input().split()))
 
+# lis: 연속적으로 증가하는 부분수열의 길이(1로 초기화)
 lis = [1 for _ in range(max(children) + 1)]
+# children_set: 아이들의 번호를 담을 셋
 children_set = set()
+
 for i in range(N):
+    # 아이의 번호를 셋에 넣는다.
     children_set.add(children[i])
+    # 직전 번호가 셋에 존재하면 lis 길이 갱신
     if children[i] - 1 in children_set:
         lis[children[i]] = lis[children[i] - 1] + 1
-    
+
+# 아이들 전체 명 수에서 가장 긴 연속적으로 증가하는 부분수열의 길이를 빼준다.
 print(N - max(lis))
