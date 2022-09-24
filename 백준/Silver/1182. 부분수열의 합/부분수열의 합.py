1e8bee436d5da654555d2274e5f1b14a872c6ece
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-24 21:21:34 +0900

    [Silver II] Title: 부분수열의 합, Time: 164 ms, Memory: 115928 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -4,36 +4,41 @@
 
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
 
+# N: 수열의 길이, S: 찾고자 하는 수
 N, S = map(int, input().split())
+# numbers: 수열
 numbers = list(map(int, input().split()))
-numbers.sort()
 
-subsequence = []
+# cnt: 합이 s가 되는 부분수열의 개수
 cnt = 0
 
-def backtracking(current, N, S, K):
 
+# 백트래킹 함수 선언
+def backtracking(current,total, S):
+
+    # 글로벌 변수 선언
     global cnt
 
-    if S >= 0:
-        if sum(subsequence) > S:
-            return
-
-    if len(subsequence) == K:
-        if sum(subsequence) == S:
+    # 원소를 다 선택했을 때
+    if current == N:
+        # 부분수열의 합이 s면 정답을 세준다.
+        if total == S:
             cnt += 1
-            return
+        return
 
-    for i in range(current, N):
-        subsequence.append(numbers[i])
+    # 주어진 수열에 대해서 해당 수를 선택하지 않은 경우 재귀적 탐색
+    backtracking(current + 1, total, S)
+    # 주어진 수열에 대해서 해당 수를 선택한 경우 재귀적 탐색
+    backtracking(current + 1, total + numbers[current], S) 
 
-        backtracking(i + 1, N, S, K)
 
-        subsequence.pop()
+# 0부터 백트래킹 실시
+backtracking(0, 0, S)
 
-for i in range(1, N + 1):
-    backtracking(0, N, S, i)
-
-print(cnt)
+# 정답 출력
+# S가 0이면 공집합 1개를 빼준다.
+if S == 0:
+    print(cnt - 1)
+else:
+    print(cnt)
