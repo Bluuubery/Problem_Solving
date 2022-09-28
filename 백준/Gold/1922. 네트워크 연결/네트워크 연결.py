4770956f7c5f7e1d07f89fffb0e64e4e37e26ff2
Sun <109324637+Bluuubery@users.noreply.github.com>
2022-09-28 22:14:13 +0900

    [Gold IV] Title: 네트워크 연결, Time: 368 ms, Memory: 44932 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,63 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+# 220928 1922 네트워크 연결
+
+# 정답코드
+
+# V: 정점 개수, E: 간선 개수
+V = int(input())
+E = int(input())
+# parent: 부모 테이블 (자기 자신으로 초기화)
+parent = [i for i in range(V + 1)]
+
+
+# find 함수
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+# union 함수
+def merge(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x < y:
+        parent[y] = x
+        
+    else:
+        parent[x] = y
+
+    return
+
+# edges: 간선 정보를 담을 리스트, total_cost: 최소 신장 트리의 가중치 합계
+edges = []
+total_cost = 0
+
+# 간선 정보 입력 
+for _ in range(E):
+    node1, node2, cost = map(int, input().split())
+    edges.append((cost, node1, node2))
+
+# 가중치 기준으로 정렬
+edges.sort()
+
+
+# 각 간선에 대해 유니온-파인드로 cylce 여부 판단하여 크루스칼 알고리즘 수행
+for i in range(E):
+    cost, node1, node2 = edges[i]
+    # 부모 노드가 다르면 cylce하지 않다
+    if find(node1) != find(node2):
+        # mst에 포함
+        merge(node1, node2)
+        # 가중치 계산
+        total_cost += cost
+
+# 가중치 합계 출력
+print(total_cost)
