afe34bb93e6337c73d616eccb3c09d60a54d3b30
Sun <zephyr72836@gmail.com>
2022-10-13 11:12:46 +0900

    [Gold III] Title: 아기 상어, Time: 296 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,4 +1,3 @@
-from collections import deque
 import sys, os, io, atexit
 
 input = lambda: sys.stdin.readline().rstrip('\r\n')
@@ -6,36 +5,36 @@ stdout = io.BytesIO()
 sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
 atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-# 221012 16236 아기상어
+from collections import deque
 
-# 정답코드
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
 
 
-def bfs(r, c):
-    visited = [[0 for _ in range(N)] for _ in range(N)]
-
-    visited[r][c] = 1
+def bfs(shark_r, shark_c):
+    visited = [[inf for _ in range(N)] for _ in range(N)]
+    visited[shark_r][shark_c] = 0
 
     queue = deque()
-    queue.append((r, c))
+    queue.append((shark_r, shark_c))
 
     while queue:
         r, c = queue.popleft()
-
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < N:
-                if visited[nr][nc] == 0:
-                    if arr[nr][nc] == 9:
-                        return visited[r][c]
+            if 0 <= nr < N and 0 <= nc < N and visited[nr][nc] == inf:
+                if arr[nr][nc] <= shark:
+                    visited[nr][nc] = visited[r][c] + 1
+                    queue.append((nr, nc))
+    
+    result = [(inf, inf, inf)]
+    for r in range(N):
+        for c in range(N):
+            if 0 < arr[r][c] < shark:
+                result.append((visited[r][c], r, c))
 
-                    if arr[nr][nc] <= shark:
-                        visited[nr][nc] = visited[r][c] + 1
-                        queue.append((nr, nc))
-
-    return float('inf')
-
+    return min(result)
 
 
 N = int(input())
@@ -45,46 +44,28 @@ for r in range(N):
     for c in range(N):
         if arr[r][c] == 9:
             shark_r, shark_c = r, c
+            arr[r][c] = 0
 
+inf = float('INF')
 shark = 2
 size_cnt = 0
-
-
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
 
 ans = 0
 
 while True:
-    distance = float('inf')
+    time, next_r, next_c = bfs(shark_r, shark_c)
 
-    for r in range(N):
-        for c in range(N):
-            if 0 < arr[r][c] < shark:
-                if bfs(r, c) < distance:
-                    next_r, next_c = r, c
-                    distance = bfs(r, c)
-
-    if distance == float('inf'):
+    if time == inf:
         break
     
-    arr[shark_r][shark_c] = 0
-    arr[next_r][next_c] = 9
+    arr[next_r][next_c] = 0
 
     shark_r, shark_c = next_r, next_c
 
-    ans += distance
-
+    ans += time
     size_cnt += 1
     if size_cnt == shark:
         shark += 1
         size_cnt = 0
-    
-    # for row in arr:
-    #     print(row)
-    # print(shark, distance)
-    # print()
 
 print(ans)
-
-
