511d865e050d1488370808c42440d6b88d987b69
Sun <zephyr72836@gmail.com>
2022-12-13 13:30:18 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    boolean[] arr = new boolean[31];
+
+    for (int i = 0; i < 28; i++) {
+      int N = Integer.parseInt(br.readLine());
+      arr[N] = true;
+    }
+
+    for (int i = 1; i < 31; i++) {
+      if (!arr[i]) {
+        System.out.println(i);
+      }
+    }
+  }
+}
