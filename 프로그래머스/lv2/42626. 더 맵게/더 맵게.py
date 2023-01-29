87e13888e03ffd0702efb50ba67b424ce9117417
Sun <zephyr72836@gmail.com>
2023-01-29 20:18:11 +0900

    [level 2] Title: 더 맵게, Time: 1669.39 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -2,12 +2,16 @@ import heapq
 
 def solution(scoville, K):
     
+    # 우선순위큐
     heapq.heapify(scoville)
-
-    def mix(scoville):        
+    
+    # 섞기
+    def mix(scoville):    
+        # 가장 덜 매운 2개 우선순위큐로 추출
         first = heapq.heappop(scoville)
         second = heapq.heappop(scoville)
         
+        # 섞기
         heapq.heappush(scoville, first + second * 2)
         
         return scoville
@@ -16,7 +20,7 @@ def solution(scoville, K):
     
     while True:
 
-                        
+        # 음식이 하나밖에 남지 않았거나  
         if len(scoville) == 1 and K > scoville[0]:
             answer = -1
             break
