6b6022e479bbea2c15df89ddead85e7b279b12ee
Sun <zephyr72836@gmail.com>
2023-01-29 20:36:57 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,8 +1,15 @@
 def solution(array, commands):
     answer = []
+    
     for command in commands:
+        
+        # 자르기
         temp = array[command[0] - 1: command[1]]
+        
+        # 정렬하기
         temp.sort()
+        
+        # 정답 넣어주기
         answer.append(temp[command[2] - 1])
     
     return answer
