580476aaaaf24d3663d829e389760d25f5efd1d1
Sun <zephyr72836@gmail.com>
2023-01-29 20:26:21 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -4,23 +4,28 @@ import heapq
 def solution(operations):
     answer = [0, 0]
     heap = []
-
+    
+    
     for operation in operations:
-
+        
+        # 삽입 연산자
         if operation[0] == 'I':
             heapq.heappush(heap, int(operation[2:]))
         
+        # 삭제연산자
         else:
             if not heap:
                 continue
-
+            
+            # 최솟값 삭제
             if operation[2] == '-':
                 heapq.heappop(heap)
+            # 최댓값 삭제
             else:
                 heap.remove(max(heap))
-
+    
+    # 정답 반환
     if heap: 
         answer = [max(heap), min(heap)]
 
-    # print(heap, answer)
     return answer
