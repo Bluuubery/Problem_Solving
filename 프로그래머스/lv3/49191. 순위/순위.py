741345d121ec51efdaef5980ffdb6ee45d4f7b47
Sun <zephyr72836@gmail.com>
2023-01-30 21:13:57 +0900

    [level 3] Title: 순위, Time: 19.68 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -1,3 +1,4 @@
+# dfs 함수 선언
 def dfs(node, graph, visited):
 
     visited[node] = 1
@@ -10,12 +11,15 @@ def dfs(node, graph, visited):
         visited[next] = 1
         dfs(next, graph, visited)
 
-    return sum(visited)
+    # 방문한 노드 (승자 or 패자) 수 반환
+    return sum(visited) - 1
 
 
 def solution(n, results):
+
     answer = 0
 
+    # 승자, 패자 그래프
     win_graph = [[] for _ in range(n + 1)]
     lose_graph = [[] for _ in range(n + 1)]
 
@@ -24,15 +28,20 @@ def solution(n, results):
 
         win_graph[win].append(lose)
         lose_graph[lose].append(win)
-    
+
+
+    # 각 노드에 대해서 dfs로 파악된 승자와 패자 수 구하기
     for i in range(1, n + 1):
+
+        # 자신 밑으로 패자의 수
         visited = [0 for _ in range(n + 1)]
-        win_cnt = dfs(i, win_graph, visited) - 1
+        win_cnt = dfs(i, win_graph, visited)
 
-
+        # 자신 위로 승자의 수
         visited = [0 for _ in range(n + 1)]
-        lose_cnt = dfs(i, lose_graph, visited) - 1
+        lose_cnt = dfs(i, lose_graph, visited) 
 
+        # 순서가 확정난 경우 
         if win_cnt + lose_cnt == n - 1:
             answer += 1
 
