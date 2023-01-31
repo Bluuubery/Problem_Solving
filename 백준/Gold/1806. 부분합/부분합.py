d5337d1553d6c4f0961d6af4d4edb462eac60456
Sun <zephyr72836@gmail.com>
2023-01-31 21:24:41 +0900

    [Gold IV] Title: 부분합, Time: 152 ms, Memory: 125300 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -12,30 +12,35 @@ atexit.register(lambda: os.write(1, stdout.getvalue()))
 N, M = map(int, input().split())
 numbers = list(map(int, input().split()))
 
+# 투포인터
 left = 0
 right = 0
+
+# 부분합
 sum_num = numbers[0]
 
+# 최소길이 선언 및 초기화
 min_len = sys.maxsize
 
 while True:
     
+    # 부분합이 M보다 클 경우 수열 길이 줄이기
     if sum_num >= M:
         sum_num -= numbers[left]
         min_len = min(min_len, right - left + 1)
         
         left += 1
 
+    # 작을 경우 수열 길이 늘리기
     else:
         right += 1
 
+        # 탐색 완료
         if right == N:
             break
 
         sum_num += numbers[right]
     
-    # print(left, right, sum_num, min_len)
-
 
 if min_len == sys.maxsize:
     print(0)
