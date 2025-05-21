6c2d55bb8d48f8b100a5cae49e6516132e98f656
Sun <zephyr72836@gmail.com>
2025-05-21 22:55:22 +0900

    [level 2] Title: 서버 증설 횟수, Time: 49.21 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+		
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+        
+		int[] capability = new int[24];
+        
+        for (int i=0; i < players.length; i++) {
+			
+			// 사용자 수가 현재 가용량보다 적을 경우
+            if (players[i] >= m && players[i] > capability[i]){
+                
+				// 증설 서버 개수
+				int addedServer = (players[i] - capability[i]) / m;
+                answer += addedServer;
+				// 지속 시간(k)만큼 증설 유지
+				for(int j=0; j<k; j++){
+                    if ((j + i) >= players.length) break;
+					capability[j + i] += addedServer * m;
+				}
+                System.out.println(i + ": add " + addedServer );
+			}
+            			System.out.println(i + ": " + Arrays.toString(capability));
+
+            
+        }
+        
+        return answer;
+    }
+}
